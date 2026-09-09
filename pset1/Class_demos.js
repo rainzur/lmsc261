2a0ae3164f50b98b5cb4f6fcdefdc1459ef69d93
Rain Z <rzuraski@berklee.edu>
2026-09-09 17:44:59 -0400

    Completed homework

---
+++ pset1/Class_demos.js
@@ -0,0 +1,18 @@
+/*
+let myName = "luxurious luke"
+let greeting = "hello, " + myName;
+print(greeting);
+*/
+
+/*
+let myName = prompt("What is your name?");
+let greeting = "How do you do, " + myName;
+print(greeting);
+*/
+
+
+let myName = prompt("What is your name?");
+let myName1 = prompt("What is your name?");
+let greeting = "How do you do, " + myName + " and " + myName1;
+print(greeting);
+
---
+++ pset1/hello_someone.js
@@ -0,0 +1,2 @@
+let nameToPrint = "Jessiepoo";
+print("Hello, " + nameToPrint);
