d0642fd4f3def63b1fce7815215683c19151f191
Rain Z <rzuraski@berklee.edu>
2026-09-11 16:57:01 -0400

    notes from lmsc

---
+++ pset1/notes.md
@@ -0,0 +1,11 @@
+### Variables
+variables defined by using keyword : let 
+Variables have three types: strings, numbers, booleans
+Ternary operators use a boolean to select message.
+
+Shft cmd v switches view
+
+# Coding Class
+```
+let greeting = "helloWorld"
+```
---
+++ pset1/variables.js
@@ -0,0 +1,53 @@
+//strings --- sequence of letters and/or numbers
+/*
+let myName = "Awesome Amir";
+let nameUppercase = myName.toUpperCase();
+let nameLowerCase = myName.toLowerCase();
+print(nameLowerCase);
+*/
+//NUMBER -- integers and floats
+/*
+let myrealNumber = 10; //number 
+let fakeNumber = "10"; //string type
+*/
+
+/*
+let lukeAge = 19;//number
+let cameronAge = 19;//number
+let minName = "min";//string
+let minNameLength = minName.length//number
+print(minNameLength);
+print(lukeAge + cameronAge);
+print(lukeAge + cameronAge);
+print(lukeAge * cameronAge); 
+print(lukeAge / cameronAge);
+print(lukeAge ** cameronAge);
+*/
+//Boolean
+/*
+let victimName = "max"; // string
+let heightAfraidInFloor = 5; // number
+let currentHeight = prompt("what floor are you on?")
+let isMaxAfraid = currentHeight > heightAfraidInFloor; // boolean
+print(isMaxAfraid)
+*/
+/*
+let currentFloor = 5;
+let scaredFloor = 5;
+let isMaxAfraid = currentFloor > scaredFloor;
+
+let message = isMaxAfraid ? "Oh fuk!!" : "Whew";
+print(message)
+*/
+let semester = prompt("whats your semester?");
+let closeToGrad = 7;
+let isCloseToGrad = semester >= closeToGrad; // boolean
+let message = isCloseToGrad ? "Alcohol is not the answer" : "practice your fucking scales"
+print(message)
+/*
+const minimumHeight = 4; // number
+let myHeight = prompt("How tall are you?");
+let canRideCoaster = myHeight >= minimumHeight; // bool
+let message = canRideCoaster ? "Come on in!" : "Get lost motherfucker"
+print(message);
+*/
